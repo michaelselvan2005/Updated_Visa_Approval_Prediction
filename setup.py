1bfd5b93c46e141d814c04c973448cec1f98d192
Michael Selvan <michael.selvan2005@gmail.com>
2024-03-02 19:51:06 +0530

    installing_packages

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="use_visa",
+    version="0.0.0",
+    author="Michael",
+    author_email="michael.selvan2005@gmail.com",
+    packages=find_packages()
+)
