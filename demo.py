31c09119e92510daa60192917ee6579ece15dd2e
Michael Selvan <michael.selvan2005@gmail.com>
2024-03-03 09:45:48 +0530

    Utils Added

--- demo.py
+++ demo.py
@@ -2,6 +2,8 @@ from us_visa.components.data_ingestion import main
 #from us_visa.logger import logging
 from us_visa.exception import USvisaException
 import sys
+
+#logging.info("Welcome to our Projects !!!!")
 try:
     a = 1/ 10
     print(a)
