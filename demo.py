4c718ad64f33d0247d4fb9bb41b1c2bb98edd01d
Michael Selvan <michael.selvan2005@gmail.com>
2024-03-03 09:33:17 +0530

    Exceptions Handling

--- demo.py
+++ demo.py
@@ -1,3 +1,10 @@
 from us_visa.components.data_ingestion import main
+#from us_visa.logger import logging
+from us_visa.exception import USvisaException
+import sys
+try:
+    a = 1/ 10
+    print(a)
+except Exception as e:
+    raise USvisaException(e, sys) from e
 
-main()
